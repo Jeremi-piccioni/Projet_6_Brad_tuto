fe539acd2ee4182a6bca4cfff5e388b26dd8c1a5
Jeremi <jeremi.piccioni@gmail.com>
2021-01-19 23:52:05 +0100

    Mot de passe mongoDB a change

---
+++ congif/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://<username>:MongoDB2018@cluster0.m433z.mongodb.net/<dbname>?retryWrites=true&w=majority' // verifier le mot de passe
+}
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 
 // Login Page
 router.get('/login', (req,res) => res.send('Login page')) 
